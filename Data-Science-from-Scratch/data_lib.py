12c20d675dca04b856b81dc6d9334ac523c1efac
Federico Baldi <federico96.baldi@gmail.com>
2022-08-26 15:32:50 +0200

    Update data library

--- Data-Science-from-Scratch/data_lib.py
+++ Data-Science-from-Scratch/data_lib.py
@@ -1,5 +1,5 @@
-from typing import List, Tuple, Callable, NamedTuple, Dict, Set
-from collections import Counter
+from typing import List, Tuple, Callable, NamedTuple, Dict, Set, Any, Union
+from collections import Counter, defaultdict
 from typing import TypeVar, Iterator
 import math
 import random
@@ -698,3 +698,174 @@ def negative_log_gradient(xs: List[Vector],
                           beta: Vector) -> Vector:
     return vector_sum([_negative_log_gradient(x, y, beta)
                        for x, y in zip(xs, ys)])
+
+def entropy(class_probabilities: List[float]) -> float:
+    """Given a list of class probabilities, compute the entropy"""
+    return sum(-p * math.log(p, 2)
+               for p in class_probabilities
+               if p > 0)                     # ignore zero probabilities
+
+def class_probabilities(labels: List[Any]) -> List[float]:
+    total_count = len(labels)
+    return [count / total_count
+            for count in Counter(labels).values()]
+
+def data_entropy(labels: List[Any]) -> float:
+    return entropy(class_probabilities(labels))
+
+def partition_entropy(subsets: List[List[Any]]) -> float:
+    """Returns the entropy from this partition of data into subsets"""
+    total_count = sum(len(subset) for subset in subsets)
+    return sum(data_entropy(subset) * len(subset) / total_count
+               for subset in subsets)
+
+def partition_by(inputs: List[T], attribute: str) -> Dict[Any, List[T]]:
+    """Partition the inputs into lists based on the specified attribute."""
+    partitions: Dict[Any, List[T]] = defaultdict(list)
+    for input in inputs:
+        key = getattr(input, attribute)  # value of the specified attribute
+        partitions[key].append(input)    # add input to the correct partition
+    return partitions
+
+def partition_entropy_by(inputs: List[Any],
+                         attribute: str,
+                         label_attribute: str) -> float:
+    """Compute the entropy corresponding to the given partition"""
+    # partitions consist of our inputs
+    partitions = partition_by(inputs, attribute)
+    # but partition_entropy needs just the class labels
+    labels = [[getattr(input, label_attribute) for input in partition]
+              for partition in partitions.values()]
+    return partition_entropy(labels)
+
+class Leaf(NamedTuple):
+    value: Any
+
+class Split(NamedTuple):
+    attribute: str
+    subtrees: dict
+    default_value: Any = None
+
+DecisionTree = Union[Leaf, Split]
+
+def classify(tree: DecisionTree, input: Any) -> Any:
+    """classify the input using the given decision tree"""
+    # If this is a leaf node, return its value
+    if isinstance(tree, Leaf):
+        return tree.value
+    # Otherwise this tree consists of an attribute to split on
+    # and a dictionary whose keys are values of that attribute
+    # and whose values are subtrees to consider next
+    subtree_key = getattr(input, tree.attribute)
+    if subtree_key not in tree.subtrees:   # If no subtree for key,
+        return tree.default_value          # return the default value.
+    subtree = tree.subtrees[subtree_key]   # Choose the appropriate subtree
+    return classify(subtree, input)        # and use it to classify the input.
+
+def build_tree_id3(inputs: List[Any],
+                   split_attributes: List[str],
+                   target_attribute: str) -> DecisionTree:
+    # Count target labels
+    label_counts = Counter(getattr(input, target_attribute)
+                           for input in inputs)
+    most_common_label = label_counts.most_common(1)[0][0]
+    # If there's a unique label, predict it
+    if len(label_counts) == 1:
+        return Leaf(most_common_label)
+    # If no split attributes left, return the majority label
+    if not split_attributes:
+        return Leaf(most_common_label)
+    # Otherwise split by the best attribute
+
+    def split_entropy(attribute: str) -> float:
+        """Helper function for finding the best attribute"""
+        return partition_entropy_by(inputs, attribute, target_attribute)
+
+    best_attribute = min(split_attributes, key=split_entropy)
+    partitions = partition_by(inputs, best_attribute)
+    new_attributes = [a for a in split_attributes if a != best_attribute]
+    # Recursively build the subtrees
+    subtrees = {attribute_value : build_tree_id3(subset,
+                                                 new_attributes,
+                                                 target_attribute)
+                for attribute_value, subset in partitions.items()}
+
+    return Split(best_attribute, subtrees, default_value=most_common_label)
+
+def step_function(x: float) -> float:
+    return 1.0 if x >= 0 else 0.0
+
+def perceptron_output(weights: Vector, bias: float, x: Vector) -> float:
+    """Returns 1 if the perceptron 'fires', 0 if not"""
+    calculation = dot(weights, x) + bias
+    return step_function(calculation)
+
+def sigmoid(t: float) -> float:
+    return 1 / (1 + math.exp(-t))
+
+def neuron_output(weights: Vector, inputs: Vector) -> float:
+    # weights includes the bias term, inputs includes a 1
+    return sigmoid(dot(weights, inputs))
+
+def feed_forward(neural_network: List[List[Vector]],
+                 input_vector: Vector) -> List[Vector]:
+    """
+    Feeds the input vector through the neural network.
+    Returns the outputs of all layers (not just the last one).
+    """
+    outputs: List[Vector] = []
+    for layer in neural_network:
+        input_with_bias = input_vector + [1]              # Add a constant.
+        output = [neuron_output(neuron, input_with_bias)  # Compute the output
+                  for neuron in layer]                    # for each neuron.
+        outputs.append(output)                            # Add to results.
+        # Then the input to the next layer is the output of this one
+        input_vector = output
+    return outputs
+
+def sqerror_gradients(network: List[List[Vector]],
+                      input_vector: Vector,
+                      target_vector: Vector) -> List[List[Vector]]:
+    """
+    Given a neural network, an input vector, and a target vector,
+    make a prediction and compute the gradient of the squared error
+    loss with respect to the neuron weights.
+    """
+    # forward pass
+    hidden_outputs, outputs = feed_forward(network, input_vector)
+    # gradients with respect to output neuron pre-activation outputs
+    output_deltas = [output * (1 - output) * (output - target)
+                     for output, target in zip(outputs, target_vector)]
+    # gradients with respect to output neuron weights
+    output_grads = [[output_deltas[i] * hidden_output
+                     for hidden_output in hidden_outputs + [1]]
+                    for i, output_neuron in enumerate(network[-1])]
+    # gradients with respect to hidden neuron pre-activation outputs
+    hidden_deltas = [hidden_output * (1 - hidden_output) *
+                         dot(output_deltas, [n[i] for n in network[-1]])
+                     for i, hidden_output in enumerate(hidden_outputs)]
+    # gradients with respect to hidden neuron weights
+    hidden_grads = [[hidden_deltas[i] * input for input in input_vector + [1]]
+                    for i, hidden_neuron in enumerate(network[0])]
+    return [hidden_grads, output_grads]
+
+def fizz_buzz_encode(x: int) -> Vector:
+    if x % 15 == 0:
+        return [0, 0, 0, 1]
+    elif x % 5 == 0:
+        return [0, 0, 1, 0]
+    elif x % 3 == 0:
+        return [0, 1, 0, 0]
+    else:
+        return [1, 0, 0, 0]
+
+def binary_encode(x: int) -> Vector:
+    binary: List[float] = []
+    for i in range(10):
+        binary.append(x % 2)
+        x = x // 2
+    return binary
+    
+def argmax(xs: list) -> int:
+    """Returns the index of the largest value"""
+    return max(range(len(xs)), key=lambda i: xs[i])
